dcb27b8a292925c9f650c373233cf1904ab6e755
Filip Rudek <rudek.filip@gmail.com>
2021-10-13 17:38:59 +0200

    Injected GlobalStyles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { GlobalStyles, theme } from "../styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
